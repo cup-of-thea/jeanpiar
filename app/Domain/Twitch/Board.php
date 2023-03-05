9f8a2771feb66e79d7d03ba7fb87d6c2009125a9
Thea <thea.joy@outlook.com>
2023-03-05 03:37:27 +0100

    build: cache

--- app/Domain/Twitch/Board.php
+++ app/Domain/Twitch/Board.php
@@ -2,91 +2,39 @@
 
 namespace App\Domain\Twitch;
 
+use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\Facades\Http;
 
 class Board
 {
-    /**
-     * @throws UnauthenticatedException
-     */
     public static function information(string $login): array
     {
-        $token = (new Connect())();
-        $response = Http::withHeaders([
-            'Authorization' => "Bearer $token",
-            'Client-Id' => config('services.twitch.client'),
-        ])->get('https://api.twitch.tv/helix/users', ['login' => $login]);
+        return Cache::remember("twitch:information:$login", 3600, function () use ($login) {
+            $token = (new Connect())();
+            $response = Http::withHeaders([
+                'Authorization' => "Bearer $token",
+                'Client-Id' => config('services.twitch.client'),
+            ])->get('https://api.twitch.tv/helix/users', ['login' => $login]);
 
-        return $response->json('data')[0];
+            return $response->json('data')[0];
+        });
+
     }
 
-    /**
-     * @throws UnauthenticatedException
-     */
     public static function followers(): int
     {
-        $token = (new Connect())();
-        $response = Http::withHeaders([
-            'Authorization' => "Bearer $token",
-            'Client-Id' => config('services.twitch.client'),
-        ])
-        ->get('https://api.twitch.tv/helix/channels/followers', [
-            'first' => 1,
-            'broadcaster_id' => config('services.twitch.broadcaster'),
-        ]);
+        return Cache::remember('twitch:followers', 3600, function () {
+            $token = (new Connect())();
+            $response = Http::withHeaders([
+                'Authorization' => "Bearer $token",
+                'Client-Id' => config('services.twitch.client'),
+            ])
+                ->get('https://api.twitch.tv/helix/channels/followers', [
+                    'first' => 1,
+                    'broadcaster_id' => config('services.twitch.broadcaster'),
+                ]);
 
-        return $response->json('total');
-    }
-
-    /**
-     * @throws UnauthenticatedException
-     */
-    public static function emotes(): array
-    {
-        $token = (new Connect())();
-        $response = Http::withHeaders([
-            'Authorization' => "Bearer $token",
-            'Client-Id' => config('services.twitch.client'),
-        ])
-            ->get('https://api.twitch.tv/helix/chat/emotes', [
-                'first' => 1,
-                'broadcaster_id' => config('services.twitch.broadcaster'),
-            ]);
-
-        return $response->json('data');
-    }
-
-    /**
-     * @throws UnauthenticatedException
-     */
-    public static function clips(): array
-    {
-        $token = (new Connect())();
-        $response = Http::withHeaders([
-            'Authorization' => "Bearer $token",
-            'Client-Id' => config('services.twitch.client'),
-        ])
-            ->get('https://api.twitch.tv/helix/clips', [
-                'broadcaster_id' => config('services.twitch.broadcaster'),
-            ]);
-
-        return $response->json('data');
-    }
-
-    /**
-     * @throws UnauthenticatedException
-     */
-    public static function schedule(): array
-    {
-        $token = (new Connect())();
-        $response = Http::withHeaders([
-            'Authorization' => "Bearer $token",
-            'Client-Id' => config('services.twitch.client'),
-        ])
-            ->get('https://api.twitch.tv/helix/schedule', [
-                'broadcaster_id' => config('services.twitch.broadcaster'),
-            ]);
-
-        return $response->json('data');
+            return $response->json('total');
+        });
     }
 }
--- app/Domain/Twitch/Connect.php
+++ app/Domain/Twitch/Connect.php
@@ -2,28 +2,27 @@
 
 namespace App\Domain\Twitch;
 
+use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\Facades\Http;
 use Illuminate\Validation\UnauthorizedException;
 use JetBrains\PhpStorm\NoReturn;
 
 class Connect
 {
-    /**
-     * @throws UnauthenticatedException
-     */
     public function __invoke(): string
     {
-        $response = Http::asForm()->post('https://id.twitch.tv/oauth2/token', [
-            'client_id' => config('services.twitch.client'),
-            'client_secret' => config('services.twitch.secret'),
-            'grant_type' => 'client_credentials',
-        ]);
+        return Cache::remember('twitch:access', 604800, function () {
+            $response = Http::asForm()->post('https://id.twitch.tv/oauth2/token', [
+                'client_id' => config('services.twitch.client'),
+                'client_secret' => config('services.twitch.secret'),
+                'grant_type' => 'client_credentials',
+            ]);
 
-        if($response->ok()) {
-            return $response->json('access_token');
-        }
+            if($response->ok()) {
+                return $response->json('access_token');
+            }
 
-        throw new UnauthenticatedException();
+            throw new UnauthenticatedException();
+        });
     }
-
 }
